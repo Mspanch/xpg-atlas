2d22565f059bc57c471a7dd2752d99da4b4eed9c
Mspanch <123329394+Mspanch@users.noreply.github.com>
2025-12-18 15:29:50 -0800

    Fix Static path config so home image loads

--- xpg_portal/settings.py
+++ xpg_portal/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Required for Render collectstatic
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = []
+
 
 
 # Default primary key field type
